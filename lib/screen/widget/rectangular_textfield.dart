6e95933134ec33742895a6efee4afe8657b20658
zaiehilmi <zaiehilmi@gmail.com>
2023-03-21 17:36:46 +0800

    pembetulan

--- lib/screen/widget/rectangular_textfield.dart
+++ lib/screen/widget/rectangular_textfield.dart
@@ -2,38 +2,47 @@ import 'package:flutter/material.dart';
 
 class RectangularTextfield extends StatelessWidget {
   static const double textSize = 15;
+
   String hint;
   TextEditingController controller;
+  Color? bgColor;
+  Color? textColor;
 
   RectangularTextfield({
     super.key,
     required this.hint,
     required this.controller,
+    this.bgColor,
+    this.textColor,
   });
 
   @override
-  Widget build(BuildContext context) => Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: const Color(0xFFF06449),
+  Widget build(BuildContext context) {
+    final colorTheme = Theme.of(context).colorScheme;
+
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: bgColor,
+      ),
+      child: TextField(
+        style: TextStyle(
+          fontSize: textSize,
+          color: textColor ?? colorTheme.onPrimaryContainer,
+          fontWeight: FontWeight.normal,
         ),
-        child: TextField(
-          style: const TextStyle(
-            fontSize: textSize,
-            color: Colors.white70,
-            fontWeight: FontWeight.bold,
+        decoration: InputDecoration(
+          hintText: hint,
+          hintStyle: const TextStyle(
+              fontSize: textSize, fontWeight: FontWeight.normal),
+          border: InputBorder.none,
+          contentPadding: const EdgeInsets.symmetric(
+            horizontal: 20.0,
+            vertical: 16.0,
           ),
-          decoration: InputDecoration(
-            hintText: hint,
-            hintStyle: const TextStyle(
-                fontSize: textSize, fontWeight: FontWeight.normal),
-            border: InputBorder.none,
-            contentPadding: const EdgeInsets.symmetric(
-              horizontal: 20.0,
-              vertical: 16.0,
-            ),
-          ),
-          controller: controller,
         ),
-      );
+        controller: controller,
+      ),
+    );
+  }
 }
